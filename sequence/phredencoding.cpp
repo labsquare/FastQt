d317d99e43aeaf439aa236fcb2f991d0c4d3d90e
dridk <sacha@labsquare.org>
2017-01-11 15:48:11 +0100

    add GPL license and authors

--- sequence/phredencoding.cpp
+++ sequence/phredencoding.cpp
@@ -1,3 +1,25 @@
+/*
+Copyright Copyright 2016-17 Sacha Schutz
+
+    This file is part of CuteVCF.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+    @author : Pierre Lindenbaum from FastQC <http://www.bioinformatics.babraham.ac.uk/projects/fastqc/>
+    @author : Sacha Schutz <sacha@labsquare.org>
+    @author : Pierre Marijon <pierre@marijon.fr>
+*/
 #include "phredencoding.h"
 
 PhredEncoding::PhredEncoding(const QString& n, int offset)
