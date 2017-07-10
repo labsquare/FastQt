b38dbc32db2304fab5f1970f467be18a99e45328
Dylan Aïssi <bob.dybian@gmail.com>
2017-07-11 00:04:15 +0200

    Fix license headers

--- analysis/perbasencontentanalysis.h
+++ analysis/perbasencontentanalysis.h
@@ -1,19 +1,20 @@
 /*
-Copyright Copyright 2016-17 Pierre Marijon
-    This file is part of fastQT.
+Copyright 2016-17 Pierre Marijon
 
-    Foobar is free software: you can redistribute it and/or modify
+    This file is part of FastQt.
+
+    FastQt is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    FastQt is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with FastQt.  If not, see <http://www.gnu.org/licenses/>.
 
     @author : Pierre Lindenbaum from FastQC <http://www.bioinformatics.babraham.ac.uk/projects/fastqc/>
     @author : Sacha Schutz <sacha@labsquare.org>
