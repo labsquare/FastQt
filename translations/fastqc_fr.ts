6aaae100aad8e3fdeb9bf8d6c006312d4959730e
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2017-07-15 11:12:58 +0200

    re-add translations/fastqc_fr.ts
    (deletion mistake: commit 6da374b0eed431c8a6546bc5d2154ad956221103)

---
+++ translations/fastqc_fr.ts
@@ -0,0 +1,440 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="ui/aboutdialog.cpp" line="37"/>
+        <source>About %1</source>
+        <translation>A propos de %1</translation>
+    </message>
+</context>
+<context>
+    <name>BasicStatsAnalysis</name>
+    <message>
+        <location filename="analysis/basicstatsanalysis.cpp" line="30"/>
+        <source>Basic Stat</source>
+        <translatorcomment>On peut aussi mettre Statistiques mais plus long..</translatorcomment>
+        <translation>Stats générales</translation>
+    </message>
+    <message>
+        <location filename="analysis/basicstatsanalysis.cpp" line="83"/>
+        <source>Total Sequences</source>
+        <translation>Nombre total de séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/basicstatsanalysis.cpp" line="84"/>
+        <source>Sequence length</source>
+        <translatorcomment>Pluriel utilisé ici car le singulier est moins naturel</translatorcomment>
+        <translation>Longueur des séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/basicstatsanalysis.cpp" line="85"/>
+        <source>Encoding</source>
+        <translatorcomment>Codage peut également passer.</translatorcomment>
+        <translation>Encodage</translation>
+    </message>
+</context>
+<context>
+    <name>LengthDistributionAnalysis</name>
+    <message>
+        <location filename="analysis/lengthdistributionanalysis.cpp" line="5"/>
+        <source>Sequence Length Distribution</source>
+        <translatorcomment>&apos;Répartition&apos; est plus naturel que &apos;distribution&apos; selon moi (jnsll).</translatorcomment>
+        <translation>Répartition de la longueur des séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/lengthdistributionanalysis.cpp" line="6"/>
+        <source>Shows the distribution of sequence length over all sequences</source>
+        <translation>Présente la répartition de la longueur des séquences pour l&apos;ensemble des séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/lengthdistributionanalysis.cpp" line="58"/>
+        <source>Position in read (bp)</source>
+        <translatorcomment>&apos;Séquence&apos; à la place de &apos;read&apos;? </translatorcomment>
+        <translation>Position au sein du read (bp)</translation>
+    </message>
+    <message>
+        <location filename="analysis/lengthdistributionanalysis.cpp" line="59"/>
+        <source>Sequence count</source>
+        <translation>Nombre de séquence</translation>
+    </message>
+    <message>
+        <location filename="analysis/lengthdistributionanalysis.cpp" line="70"/>
+        <source>Distribution of sequence length over all sequences</source>
+        <translation>Répartition de la longueur des séquences pour l&apos;ensemble des séquences</translation>
+    </message>
+</context>
+<context>
+    <name>MainAnalyseModel</name>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="44"/>
+        <source>Waiting</source>
+        <translation>En attente</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="45"/>
+        <source>Canceled</source>
+        <translation>Annulé</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="46"/>
+        <source>Running</source>
+        <translation>En cours</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="47"/>
+        <source>Finished</source>
+        <translation>Terminé</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="150"/>
+        <source>Filename</source>
+        <translation>Nom du fichier</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="151"/>
+        <source>Status</source>
+        <translation>Statut</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="152"/>
+        <source>File size</source>
+        <translation>Taille du fichier</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="153"/>
+        <source>Progress</source>
+        <translation>Progression</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="154"/>
+        <source>Reads</source>
+        <translation>Reads</translation>
+    </message>
+    <message>
+        <location filename="model/mainanalysemodel.cpp" line="155"/>
+        <source>Time</source>
+        <translation>Temps</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="48"/>
+        <source>Open Fastq file</source>
+        <translation>Ouvrir les fichiers fastq</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="48"/>
+        <source>Fastq Extension (*.fq *.fq.* *.fastq *.fastq.* *.trim *.trim.*);; Any Extension (*)</source>
+        <translation>Format Fastq (*.fq *.fq.* *.fastq *.fastq.* *.trim *.trim.*);; Tout Format (*)</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="57"/>
+        <source>Running on %1 threads</source>
+        <translation>Exécution sur %1 threads </translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="63"/>
+        <location filename="ui/mainwindow.cpp" line="74"/>
+        <location filename="ui/mainwindow.cpp" line="85"/>
+        <source>Warning</source>
+        <translation>Avertissement</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="64"/>
+        <source>Are you sure you want to delete selected analysis ?</source>
+        <translation>Êtes-vous sûr de vouloir supprimer l&apos;analyse sélectionnée ?</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="75"/>
+        <source>Are you sure you want to stop selected analysis ?</source>
+        <translation>Êtes-vous sûr de vouloir arrêter l&apos;analyse sélectionnée ?</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="86"/>
+        <source>Are you sure you want to remove all analysis?</source>
+        <translation>Êtes-vous sûr de vouloir supprimer toutes les analyses ?</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="120"/>
+        <source>Cannot export</source>
+        <translation>Export impossible</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="120"/>
+        <source>Your selection is empty</source>
+        <translation>Votre sélection est vide</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="124"/>
+        <source>Select a directory to save results</source>
+        <translation>Sélectionnez un répertoire pour sauvegarder les résultats</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="135"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="136"/>
+        <source>&amp;Add files</source>
+        <translation type="unfinished">&amp;Ajouter</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="137"/>
+        <source>&amp;Export selection</source>
+        <translation>&amp;Exporter la sélection</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="140"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="144"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Editer</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="145"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;Supprimer</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="146"/>
+        <source>&amp;Stop</source>
+        <translation>&amp;Arrêter</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="147"/>
+        <source>&amp;Clear all</source>
+        <translation>&amp;Tout nettoyer</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="149"/>
+        <source>&amp;Select all</source>
+        <translation>&amp;Tout sélectionner</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="153"/>
+        <source>&amp;View</source>
+        <translation>&amp;Visualiser</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="154"/>
+        <source>&amp;Show analysis</source>
+        <translation>&amp;Voir l&apos;analyse</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="160"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="161"/>
+        <source>About %1</source>
+        <translation>A propos de %1</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="162"/>
+        <source>About &amp;Qt</source>
+        <translation>A propos de &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="165"/>
+        <source>Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+</context>
+<context>
+    <name>OverRepresentedSeqsAnalysis</name>
+    <message>
+        <location filename="analysis/overrepresentedseqsanalysis.cpp" line="7"/>
+        <source>Overrepresented sequences</source>
+        <translation>Séquences surreprésentées</translation>
+    </message>
+    <message>
+        <location filename="analysis/overrepresentedseqsanalysis.cpp" line="8"/>
+        <source>Identifies sequences which are overrepresented in the set</source>
+        <translation>Identifie les séquences qui sont surreprésentées dans le jeu de données</translation>
+    </message>
+</context>
+<context>
+    <name>PerBaseContentAnalysis</name>
+    <message>
+        <location filename="analysis/perbasecontentanalysis.cpp" line="30"/>
+        <location filename="analysis/perbasecontentanalysis.cpp" line="132"/>
+        <source>Per Base content</source>
+        <translation>Contenu en base</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasecontentanalysis.cpp" line="31"/>
+        <source>Per base content</source>
+        <translation>Contenu en base</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasecontentanalysis.cpp" line="113"/>
+        <source>Position in read (bp)</source>
+        <translation>Position au sein du read (bp)</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasecontentanalysis.cpp" line="114"/>
+        <source>Percent of Base (%)</source>
+        <translation>Pourcentage en base (%)</translation>
+    </message>
+</context>
+<context>
+    <name>PerBaseNContentAnalysis</name>
+    <message>
+        <location filename="analysis/perbasencontentanalysis.cpp" line="30"/>
+        <location filename="analysis/perbasencontentanalysis.cpp" line="99"/>
+        <source>Per Base N content</source>
+        <translation>Contenu en base N</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasencontentanalysis.cpp" line="31"/>
+        <source>Per base N content</source>
+        <translation>Contenu en base N</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasencontentanalysis.cpp" line="90"/>
+        <source>Position in read (bp)</source>
+        <translation>Position au sein du read (bp)</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasencontentanalysis.cpp" line="91"/>
+        <source>N percent (%)</source>
+        <translation>Pourcentage de N (%)</translation>
+    </message>
+</context>
+<context>
+    <name>PerBaseQualityAnalysis</name>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="109"/>
+        <source>PerBase analysis</source>
+        <translation>Analyse en base</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="110"/>
+        <source>Per base quality analysis</source>
+        <translation>Analyse qualité des bases</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="159"/>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="169"/>
+        <source>Quality per base</source>
+        <translation>Qualité par base</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="174"/>
+        <source>Means qualities</source>
+        <translation>Qualités moyennes</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="191"/>
+        <source>Position in read(bp)</source>
+        <translation>Position au sein du read (bp)</translation>
+    </message>
+    <message>
+        <location filename="analysis/perbasequalityanalysis.cpp" line="192"/>
+        <source>Quality score (phred)</source>
+        <translation>Score de qualité (phred)</translation>
+    </message>
+</context>
+<context>
+    <name>PerSequenceGCContent</name>
+    <message>
+        <location filename="analysis/persequencegccontent.cpp" line="7"/>
+        <source>Sequence GC %</source>
+        <translation>% GC au sein des séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencegccontent.cpp" line="8"/>
+        <source>Per sequence gc %</source>
+        <translation>% GC par séquence</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencegccontent.cpp" line="83"/>
+        <source>Percent of GC (%)</source>
+        <translation>Pourcentage de GC (%)</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencegccontent.cpp" line="91"/>
+        <source>Read count(s)</source>
+        <translation>Nombre de reads</translation>
+    </message>
+</context>
+<context>
+    <name>PerSequenceQualityAnalysis</name>
+    <message>
+        <location filename="analysis/persequencequalityanalysis.cpp" line="30"/>
+        <location filename="analysis/persequencequalityanalysis.cpp" line="89"/>
+        <source>Sequence quality</source>
+        <translation>Qualité des séquences</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencequalityanalysis.cpp" line="31"/>
+        <source>Per sequence quality scores</source>
+        <translation>Scores de qualité par séquence</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencequalityanalysis.cpp" line="96"/>
+        <source>Score quality (phred)</source>
+        <translation>Score de qualité (phred)</translation>
+    </message>
+    <message>
+        <location filename="analysis/persequencequalityanalysis.cpp" line="103"/>
+        <source>Read count(s)</source>
+        <translation>Nombre de reads</translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="cli/cliparser.cpp" line="35"/>
+        <source>Create all output files in the specified output directory. Please note that this directory must exist as the program will not create it.  If this option is not set then the output file for each sequence file is created in the same directory as the sequence file which was processed.</source>
+        <translation>Génére tous les fichiers de sortie dans le répertoire spécifié. Veuillez noter que ce répertoire doit déjà exister puisque le programme ne créera pas ce dernier. Si cette option n&apos;est pas sélectionnée, les fichiers de sortie seront créés dans le même répertoire que celui du fichier des séquences correspondant.</translation>
+    </message>
+    <message>
+        <location filename="cli/cliparser.cpp" line="39"/>
+        <source>Specifies the number of files which can be processed simultaneously. Use by default all avaible CPU</source>
+        <translation>Précise le nombre de fichiers qui peuvent être traités simultanément. Utilise par défaut toutes les ressources CPU disponibles.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="analysis/basicstatsanalysis.cpp" line="90"/>
+        <source>%GC</source>
+        <translation>% en GC</translation>
+    </message>
+</context>
+<context>
+    <name>ResultsModel</name>
+    <message>
+        <location filename="analysis/overrepresentedseqsanalysis.cpp" line="206"/>
+        <source>Sequence</source>
+        <translation>Séquence</translation>
+    </message>
+    <message>
+        <location filename="analysis/overrepresentedseqsanalysis.cpp" line="209"/>
+        <source>Count</source>
+        <translation>Décompte</translation>
+    </message>
+    <message>
+        <location filename="analysis/overrepresentedseqsanalysis.cpp" line="212"/>
+        <source>Percentage</source>
+        <translation>Pourcentage</translation>
+    </message>
+</context>
+<context>
+    <name>files</name>
+    <message>
+        <location filename="cli/cliparser.cpp" line="32"/>
+        <source>List of fastq files</source>
+        <translation>Liste des fichiers fastq</translation>
+    </message>
+</context>
+</TS>
